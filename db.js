2fc9700953c5eba08722484c9a6d94b05a3883a6
Chelsea Kramer <chelsea.kramer99@gmail.com>
2020-02-26 17:04:01 -0500

    ready-to-use

--- db.js
+++ db.js
@@ -1,25 +1,66 @@
-const pg = require("pg")
-const { Client } = pg
-const uuid = require("uuid/v4")
-const client = new Client("postgres://localhost/acme_dictionary")
-const faker = require("faker")
+const pg = require("pg");
+const { Client } = pg;
+const uuid = require("uuid/v4");
+const client = new Client(
+  process.env.DATABASE_URL || "postgres://localhost/boilerplatedb"
+);
+const faker = require("faker");
 
-client.connect()
+client.connect();
 
 const sync = async () => {
   const SQL = `    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-  DROP TABLE IF EXISTS table_name;
-  CREATE TABLE table_name
+  DROP TABLE IF EXISTS user_things;
+  DROP TABLE IF EXISTS things;
+  DROP TABLE IF EXISTS users;
+
+  CREATE TABLE users
   (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    name VARCHAR NOT NULL,
-    date_create TIMESTAMP default CURRENT_TIMESTAMP
+    name VARCHAR(100) NOT NULL,
+    date_create TIMESTAMP default CURRENT_TIMESTAMP,
+    CHECK (char_length(name) > 0)
   );
-  INSERT INTO table_name (name) VALUES ('word');
-  `
-  await client.query(SQL)
-}
+  CREATE TABLE things (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name VARCHAR(100) NOT NULL,
+    CHECK (char_length(name) > 0)
+  );
+  CREATE TABLE user_things (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    "thingId" UUID REFERENCES things(id),
+    "userId" UUID REFERENCES users(id),
+    "isFavorite" BOOLEAN DEFAULT FALSE
+  );
+  CREATE UNIQUE INDEX ON user_things("thingId", "userId");
+  INSERT INTO users (name) VALUES ('userName');
+  `;
+  await client.query(SQL);
+};
+//////////////////get///////////////////
+// const readUsers = async () => {
+//   const SQL = `SELECT * FROM users;`;
+//   const response = await client.query(SQL);
+//   return response.rows;
+// };
+//////////////////post///////////////////
+// const createUser = async ({ name }) => {
+//   const SQL = `INSERT INTO users (name) VALUES ($1) returning *;`;
+//   const response = await client.query(SQL, [name]);
+//   return response.rows[0];
+// };
+//////////////////put///////////////////
+// const updateUserThing = async ({ isFavorite, id }) => {
+//   const SQL = `UPDATE user_things SET (isFavorite) = ($1) WHERE (id) = ($2) returning *`;
+//   const response = await client.query(SQL, [isFavorite, id]);
+//   return response.rows[0];
+// };
+//////////////////delete///////////////////
+// const deleteUser = async id => {
+//   const SQL = `DELETE FROM users WHERE (id) = ($1);`;
+//   await client.query(SQL, [id]);
+// };
 
 module.exports = {
   sync
-}
+};
--- server.js
+++ server.js
@@ -1,25 +1,56 @@
-const express = require("express")
-const app = express()
-const router = express.Router()
-const path = require("path")
-const morgan = require("morgan")
-const fs = require("fs")
-const db = require("./db")
-const bodyParser = require("body-parser")
+const express = require("express");
+const app = express();
+const router = express.Router();
+const path = require("path");
+const morgan = require("morgan");
+const fs = require("fs");
+const db = require("./db");
+const bodyParser = require("body-parser");
+const port = process.env.PORT || 3000;
 
-const port = 3000
+//////////////////use///////////////////
+app.use(express.json());
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms")
+);
+app.use(bodyParser.json());
+app.use(express.static("assets"));
 
-app.use(morgan(":method :url :status :res[content-length] - :response-time ms"))
-app.use(bodyParser.json())
-
-app.use(express.static("assets"))
+//////////////////get////////////////////
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/index.html"))
-})
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+// app.get("/api/users", async (req, res, next) => {
+//   await db
+//     .readUsers()
+//     .then(users => res.send(users))
+//     .catch(next);
+// });
+
+//////////////////post////////////////////
+// app.post("/api/users", (req, res, next) => {
+//   db.createUser(req.body)
+//     .then(user => res.send(user))
+//     .catch(next);
+// });
+
+///////////////////put////////////////////
+// app.put("/api/user_things/:id", (req, res, next) => {
+//   db.updateUserThings(req.body)
+//     .then(userThing => res.send(userThing))
+//     .catch(next);
+// });
+
+//////////////////delete////////////////////
+// app.delete("/api/users/:id", (req, res, next) => {
+//   db.deleteUser(req.params.id)
+//     .then(() => res.sendStatus(204)) //since no return
+//     .catch(next);
+// });
 
 db.sync()
   .then(() => {
-    console.log("db synced")
-    app.listen(port, () => console.log(`listening on port ${port}`))
+    console.log("db synced");
+    app.listen(port, () => console.log(`listening on port ${port}`));
   })
-  .catch(ex => console.log(ex))
+  .catch(ex => console.log(ex));
