07228a263ee79c7e40d135d130d79cbfdc52a1ad
Kelli Warner <kbwarner@gmail.com>
2020-04-11 12:55:52 -0400

    fixed auth issue

--- src/Components/FindPlayers.js
+++ src/Components/FindPlayers.js
@@ -19,60 +19,64 @@ const FindPlayers = ({ users, user, setUser, auth }) => {
     ).toFixed(1);
   };
 
-  return (
-    <div className="findPlayersPage">
-      <form id="findPlayersForm">
-        <h3>Find Players</h3>
-        <input type="text" placeholder="Search for a Game" />
-        {/*
+  if (auth.id) {
+    return (
+      <div className="findPlayersPage">
+        <form id="findPlayersForm">
+          <h3>Find Players</h3>
+          <input type="text" placeholder="Search for a Game" />
+          {/*
           INPUT NEEDS AUTO-SUGGEST/COMPLETE DROPDOWN OPTIONS BASED ON ALL GAME NAMES THAT MATCH FIELD INPUT;
           */}
-        <h6>-- or --</h6>
-        <select>
-          <option>Pick a Favorite Game</option>
-          {/*
+          <h6>-- or --</h6>
+          <select>
+            <option>Pick a Favorite Game</option>
+            {/*
           LIST OF OPTIONS BASED ON TITLES OF USER'S FAVORITE GAMES
           */}
-        </select>
-        <select className="select">
-          <option>Search Distance</option>
-          {/*
+          </select>
+          <select className="select">
+            <option>Search Distance</option>
+            {/*
           LIST OF OPTIONS FOR VARYING DISTANCES
           */}
-        </select>
-        <h6>
-          <a href="">Advanced Search</a>
-        </h6>
-        <button className="searchButton">
-          <h5>Search</h5>
-        </button>
-      </form>
-      {/*
+          </select>
+          <h6>
+            <a href="">Advanced Search</a>
+          </h6>
+          <button className="searchButton">
+            <h5>Search</h5>
+          </button>
+        </form>
+        {/*
           ADVANCED SEARCH FORM DISPLAYS WHEN PROMPT IS CLICKED
           FORM CONTAINS VARIOUS SELECTORS, CHECKBOXES, RADIOS, ETC TO ALLOW THE USER TO ADJUST SEARCH PARAMETERS BASED ON GAME TYPE, GENRE, PLAYER NUMBERS, ETC
           */}
 
-      <ul id="playersList">
-        {/*
+        <ul id="playersList">
+          {/*
           LIST OF PLAYERS THAT MATCH SEARCH PARAMETERS.
           INCLUDES PROFILE IMAGE, USERNAME, DISTANCE FROM USER, MUTUAL FRIENDS/GAMES, AND 'ADD FRIEND' BUTTON
           LIST ITEMS LINK TO USER PROFILES
           */}
-        {users.map((mapUser) => {
-          if (mapUser.id != auth.id) {
-            return (
-              <li key={mapUser.id}>
-                <Link to="/chat" onClick={() => setUser(mapUser)}>
-                  {mapUser.firstname + mapUser.lastname}
-                </Link>
-                is {findDistance(mapUser)} miles away
-              </li>
-            );
-          }
-        })}
-      </ul>
-    </div>
-  );
+          {users.map((mapUser) => {
+            if (mapUser.id != auth.id) {
+              return (
+                <li key={mapUser.id}>
+                  <Link to="/chat" onClick={() => setUser(mapUser)}>
+                    {mapUser.firstname + mapUser.lastname}
+                  </Link>
+                  is {findDistance(mapUser)} miles away
+                </li>
+              );
+            }
+          })}
+        </ul>
+      </div>
+    );
+  } else {
+    return <div>Please log in to find players!</div>;
+  }
 };
 
 export default FindPlayers;
