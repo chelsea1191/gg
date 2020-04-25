f8fc3aae9bfa49086393797ca0f62d8a7de269e9
Brandon Nall <firewalkwithb@gmail.com>
2020-04-25 01:38:50 -0400

    commit

--- src/components/UserFriendsPage.js
+++ src/components/UserFriendsPage.js
@@ -9,52 +9,7 @@ const UserFriendsPage = ({
   auth,
   setUserView,
 }) => {
-  const addFriend = async () => {
-    const friendshipsCopy = [...friendships];
-    const newFriendship = await Axios.post('/api/friendships', {
-      userId: auth.id,
-      friendId: user.id,
-    }).data;
-    const friendRequests = friendships.filter(
-      (friendship) =>
-        friendship.userId === user.id && friendship.friendId === auth.id
-    );
-    if (friendRequests.length === 1) {
-      const friendRequestCopy = { ...friendRequests[0] };
-      const friendRequestIndex = friendships.indexOf(friendRequestCopy);
-      friendRequestCopy.status === 'confirmed';
-      await Axios.put(
-        `/api/friendships/${friendRequestCopy.id}`,
-        friendRequestCopy
-      );
-      friendshipsCopy.splice(friendRequestIndex, 1, friendRequestCopy);
-      newFriendship.status = 'confirmed';
-    }
-
-    setFriendships([...friendshipsCopy, newFriendship]);
-  };
-
-  const confirmedFriendships = friendships.filter(
-    (friendship) =>
-      friendship.userId === user.Id && friendship.status === 'confirmed'
-  );
-
-  const userFriendsList = confirmedFriendships.map((friendship) => {
-    const friend = users.find((user) => user.id === friendship.userId);
-
-    return (
-      <li key={friend.id} className="friendsListItem">
-        <Link to={`/users/${friend.id}`} onClick={(ev) => setUserView(friend)}>
-          <h5>{friend.username}</h5>
-        </Link>
-
-        <button type="button" onClick={addFriend}>
-          <h5>Add Friend</h5>
-        </button>
-        <hr className="hr" />
-      </li>
-    );
-  });
+  let userFriendsList = [];
 
   return (
     <div id="userFriendsPage">
