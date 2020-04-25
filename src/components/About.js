a9fe86ee75aaf174ae3e11f8bf2b8c0033c73fa5
Chelsea Kramer <chelsea.kramer99@gmail.com>
2020-04-25 10:01:59 -0400

    saturday merge

--- src/components/About.js
+++ src/components/About.js
@@ -6,24 +6,26 @@ const About = () => {
 
   const greentext = { color: 'rgb(0, 200, 0)' };
   return (
-    <div id='aboutPage'>
-      <h3>
-        Developed by <span style={greentext}>Team Awesome</span>
-      </h3>
-      <hr className='hr' />
-      <p>
-        <b style={greentext}>gg</b> allows users to connect with people near
-        them to play games.
-      </p>
-      <p>
-        Add your Favorite Games to your profile from the Games page{' '}
-        <img src='../../assets/search.png' style={iconStyle} />.
-      </p>
-      <p>
-        Use the Home page to search for other users in your area based on their
-        Favorite Games
-      </p>
-      <p>View their profiles to become Friends and chat!</p>
+    <div>
+      <div id='aboutPage'>
+        <h3>
+          Developed by <span style={greentext}>Team Awesome</span>
+        </h3>
+        <hr className='hr' />
+        <p>
+          <b style={greentext}>gg</b> allows users to connect with people near
+          them to play games.
+        </p>
+        <p>
+          Add your Favorite Games to your profile from the Games page{' '}
+          <img src='../../assets/search.png' style={iconStyle} />.
+        </p>
+        <p>
+          Use the Home page to search for other users in your area based on
+          their Favorite Games
+        </p>
+        <p>View their profiles to become Friends and chat!</p>
+      </div>
     </div>
   );
 };
