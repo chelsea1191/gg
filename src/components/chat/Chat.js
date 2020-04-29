dbada920ae51770bcabc8c3fae7a3846c162fd6c
cmd08010 <colleendunion@gmail.com>
2020-04-29 01:31:57 -0400

    got the friends stuff to work the only bug is there is the online dots show as many dots as friends because its mapping over the friends

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -35,92 +35,58 @@ const Chat = ({ auth }) => {
 
   useEffect(() => {
     axios.get(`/api/friendships/${auth.id}`).then((response) => {
-      //console.log(response, 'these are the friends so far')
-      response.data.map((res) => {
+      console.log(response, 'these are the friends so far')
+      response.data.map(async (res) => {
         if (res.userId != auth.id) {
-          axios.get(`/api/user/${res.userId}`).then((response) => {
-            console.log(response)
+          const response = await axios.get(`/api/user/${res.userId}`)
+          friendArray.push({
+            friend: response.data,
+            status: res.sendStatus,
           })
         } else if (res.friendId != auth.id) {
-          axios.get(`/api/user/${res.friendId}`).then((response) => {
-            console.log(response)
+          const response_1 = await axios.get(`/api/user/${res.friendId}`)
+          friendArray.push({
+            friend: response_1.data,
+            status: res.sendStatus,
           })
         }
+        console.log(friendArray, 'got my friendArray')
+        setFriends([...friendArray])
       })
     })
-    // friendIdArray.push({
-    //   id: res.userId,
-    //   status: res.sendStatus,
-    // })
-    // console.log(friendIdArray, 'friend array in if')
-    // console.dir(friendIdArray)
-
-    // console.log(friendIdArray, 'friend array in if')
-    // console.dir(friendIdArray)
-    // friendIdArray.push([
-    //   {
-    //     id: res.friendId,
-    //     status: res.sendStatus,
-    //   },
-    // ])
-    //     }
-    //   })
-    //   console.log(friendIdArray, 'the friends got added to this id array')
-    //   console.dir(friendIdArray, 'the friends got added to this id array')
-    //   setFriendId([...friendIdArray])
-    // })
   }, [])
-
-  // useEffect(() => {
-  //   console.log(friendId, 'third useeffect')
-  //   console.dir(friendId, 'third useeffect')
-  //   friendIdArray.map((friendId) => {
-  //     console.log(friendId)
-  //     axios.get(`/api/user/${friendId.id}`).then((response) => {
-  //       friendArray.push([
-  //         {
-  //           id: response.data.id,
-  //           username: response.data.username,
-  //           online: response.data.isOnline,
-  //           status: res.sendStatus,
-  //         },
-  //       ])
-  //     })
-  //   })
-  //   setFriends([...friendArray])
-  //   //ß  console.log(friendArray, 'after my pushes')
-  // }, [chats])
 
   if (!chats || chats.length === 0) {
     return (
       <div id="chatPage">
         <h3>Chat</h3>
         <Link to="/findplayers">Find some new players to chat with!</Link>
-        Or Friends
-        {friends.map((friend) => {
-          console.log(friend.friend)
-          if (friend.friend.id != auth.id) {
-            return (
-              <div key={friend.friend.id}>
-                <span>
-                  <Link to={`/chat/${friend.friend.id}`}>
-                    {friend.friend.username}
-                    {friend.friend.isOnline ? (
-                      <span className="dot-green"></span>
-                    ) : (
-                      <span className="dot-red"></span>
-                    )}
-                  </Link>
-                  <div>
-                    {friend.status === 'sent'
-                      ? 'Friendship not confirmed yet'
-                      : ''}
-                  </div>
-                </span>
-              </div>
-            )
-          }
-        })}
+        <div>
+          Or Friends
+          {friends.map((friend) => {
+            if (friend.friend.id != auth.id) {
+              return (
+                <div key={friend.friend.id}>
+                  <span>
+                    <Link to={`/chat/${friend.friend.id}`}>
+                      {friend.friend.username}
+                      {friend.friend.isOnline ? (
+                        <span className="dot-green"></span>
+                      ) : (
+                        <span className="dot-red"></span>
+                      )}
+                    </Link>
+                    <div>
+                      {friend.status === 'sent'
+                        ? 'Friendship not confirmed yet'
+                        : ''}
+                    </div>
+                  </span>
+                </div>
+              )
+            }
+          })}
+        </div>
       </div>
     )
   } else if (chats.length > 0) {
@@ -132,27 +98,33 @@ const Chat = ({ auth }) => {
         {chats.map((eachChat) => {
           return (
             <div key={eachChat.id}>
-              <Link to={`/chat/${eachChat.userid}`}>{eachChat.username}</Link>
-              {friends.map((friend) => {
-                if (friend.friend.id === eachChat.userId) {
+              <Link to={`/chat/${eachChat.userid}`}>
+                {eachChat.username}
+                {friends.map((friend) => {
                   return (
                     <div>
-                      {friend.friend.isOnline ? (
-                        <span className="dot-green"></span>
+                      {friend.friend.id === eachChat.userid ? (
+                        <div>
+                          {friend.friend.isOnline ? (
+                            <span className="dot-green"></span>
+                          ) : (
+                            <span className="dot-red"></span>
+                          )}
+                        </div>
                       ) : (
-                        <span className="dot-red"></span>
+                        ''
                       )}
                     </div>
                   )
-                }
-              })}
+                })}
+              </Link>
+
               <hr></hr>
             </div>
           )
         })}
         Friends:
         {friends.map((friend) => {
-          console.log(friend.friend)
           if (friend.friend.id != auth.id) {
             return (
               <div key={friend.friend.id}>
@@ -188,28 +160,3 @@ const Chat = ({ auth }) => {
 }
 
 export default Chat
-
-// return (
-//   <div id="chatPage">
-//     <hr></hr>
-//     Chat with Friends:
-//     {friends.map((friend) => {
-//       if (friend.id != auth.id) {
-//         return (
-//           <div key={friend.friendId}>
-//             <span>
-//               <Link to={`/chat/${friend.friendId}`}>
-//                 {friend.username}
-//                 {friend.isOnline ? (
-//                   <span className="dot-green"></span>
-//                 ) : (
-//                   <span className="dot-red"></span>
-//                 )}
-//               </Link>
-//             </span>
-//           </div>
-//         )
-//       }
-//     })}
-//   </div>
-// )
