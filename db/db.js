0a7b25f52dc4527687d92bf97b22c5ccf9fbfbdd
Chelsea Kramer <chelsea.kramer99@gmail.com>
2020-04-28 21:23:48 -0400

    now include prod env

--- db/db.js
+++ db/db.js
@@ -56,90 +56,89 @@ const ipsum =
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam malesuada, lacus at blandit rutrum, enim sapien pulvinar quam, vel euismod neque lectus eu ante. Ut vel congue justo, eget pharetra orci. Proin sagittis tortor elementum nunc tristique mattis.';
 
 const sync = async () => {
-  // if (process.env.NODE_ENV == 'production') {
-  //   //**********************************  PRODUCTION ******************************* */
-  //   console.log('environment is: ', process.env.NODE_ENV);
-  //   const SQL = `    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
-  // CREATE TABLE IF NOT EXISTS users (
-  //   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  //   username VARCHAR(100) NOT NULL UNIQUE,
-  //   firstname VARCHAR(100) NOT NULL,
-  //   lastname VARCHAR(100) NOT NULL,
-  //   password VARCHAR(100) NOT NULL,
-  //   role VARCHAR(20) DEFAULT 'player',
-  //   email VARCHAR(100) NOT NULL UNIQUE,
-  //   "isBlocked" BOOLEAN DEFAULT false,
-  //   "isOnline" BOOLEAN DEFAULT false,
-  //   CHECK (char_length(username) > 0),
-  //   photo VARCHAR,
-  //   bio VARCHAR(300),
-  //   latitude VARCHAR,
-  //   longitude VARCHAR,
-  //   "gameTypes" TEXT [],
-  //   date_created TIMESTAMP default CURRENT_TIMESTAMP,
-  //   avatar VARCHAR
-  // );
-  // CREATE TABLE IF NOT EXISTS game_type (
-  //   id SERIAL PRIMARY KEY,
-  //   gametype VARCHAR
-  // );
-  // CREATE TABLE IF NOT EXISTS game (
-  //   id VARCHAR PRIMARY KEY UNIQUE,
-  //   name VARCHAR,
-  //   "gameTypeID" INT,
-  //   description VARCHAR,
-  //   image_url VARCHAR,
-  //   min_players INT,
-  //   max_players INT,
-  //   url VARCHAR,
-  //   primary_publisher VARCHAR,
-  //   min_age INT,
-  //   year_published INT,
-  //   min_playtime INT,
-  //   max_playtime INT,
-  //   average_user_rating DECIMAL
-  // );
-  // CREATE TABLE IF NOT EXISTS favoritegames (
-  //   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  //   "userId" UUID REFERENCES users(id),
-  //   "gameId" VARCHAR REFERENCES game(id),
-  //   UNIQUE ("userId", "gameId")
-  // );
-  // CREATE TABLE IF NOT EXISTS friendships (
-  //   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  //   "userId" UUID REFERENCES users(id),
-  //   "friendId" UUID REFERENCES users(id),
-  //   "sendStatus" VARCHAR NOT NULL,
-  //   UNIQUE ("userId", "friendId")
-  // );
-  // CREATE TABLE IF NOT EXISTS user_group (
-  //   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  //   "userId" UUID REFERENCES users(id) NOT NULL,
-  //   "gameTypeID" INT REFERENCES game_type(id) NOT NULL
-  // );
-  // CREATE TABLE IF NOT EXISTS chat (
-  //   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  //   creator_id UUID REFERENCES users(id) NOT NULL,
-  //   user_id UUID REFERENCES users(id) NOT NULL,
-  //   date_create TIMESTAMP default CURRENT_TIMESTAMP,
-  //   render_creator_messages BOOLEAN DEFAULT true,
-  //   render_user_messages BOOLEAN DEFAULT true,
-  //   date_updated TIMESTAMP default CURRENT_TIMESTAMP
-  // );
-  // CREATE TABLE IF NOT EXISTS message (
-  //   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  //   chat_id UUID REFERENCES chat(id) NOT NULL,
-  //   sender_id UUID REFERENCES users(id) NOT NULL,
-  //   message VARCHAR,
-  //   date_updated TIMESTAMP default CURRENT_TIMESTAMP
-  // );
-  // `;
-  //   await client.query(SQL);
-  // } else {
-  /**********************************  DEVELOPMENT *******************************/
-  console.log('environment is: development');
-  const SQL = `    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+  if (process.env.NODE_ENV == 'production') {
+    //**********************************  PRODUCTION ******************************* */
+    console.log('environment is: ', process.env.NODE_ENV);
+    const SQL = `    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+  CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    username VARCHAR(100) NOT NULL UNIQUE,
+    firstname VARCHAR(100) NOT NULL,
+    lastname VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    role VARCHAR(20) DEFAULT 'player',
+    email VARCHAR(100) NOT NULL UNIQUE,
+    "isBlocked" BOOLEAN DEFAULT false,
+    "isOnline" BOOLEAN DEFAULT false,
+    CHECK (char_length(username) > 0),
+    photo VARCHAR,
+    bio VARCHAR(300),
+    latitude VARCHAR,
+    longitude VARCHAR,
+    "gameTypes" TEXT [],
+    date_created TIMESTAMP default CURRENT_TIMESTAMP,
+    avatar VARCHAR
+  );
+  CREATE TABLE IF NOT EXISTS game_type (
+    id SERIAL PRIMARY KEY,
+    gametype VARCHAR
+  );
+  CREATE TABLE IF NOT EXISTS game (
+    id VARCHAR PRIMARY KEY UNIQUE,
+    name VARCHAR,
+    "gameTypeID" INT,
+    description VARCHAR,
+    image_url VARCHAR,
+    min_players INT,
+    max_players INT,
+    url VARCHAR,
+    primary_publisher VARCHAR,
+    min_age INT,
+    year_published INT,
+    min_playtime INT,
+    max_playtime INT,
+    average_user_rating DECIMAL
+  );
+  CREATE TABLE IF NOT EXISTS favoritegames (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    "userId" UUID REFERENCES users(id),
+    "gameId" VARCHAR REFERENCES game(id),
+    UNIQUE ("userId", "gameId")
+  );
+  CREATE TABLE IF NOT EXISTS friendships (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    "userId" UUID REFERENCES users(id),
+    "friendId" UUID REFERENCES users(id),
+    "sendStatus" VARCHAR NOT NULL,
+    UNIQUE ("userId", "friendId")
+  );
+  CREATE TABLE IF NOT EXISTS user_group (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    "userId" UUID REFERENCES users(id) NOT NULL,
+    "gameTypeID" INT REFERENCES game_type(id) NOT NULL
+  );
+  CREATE TABLE IF NOT EXISTS chat (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    creator_id UUID REFERENCES users(id) NOT NULL,
+    user_id UUID REFERENCES users(id) NOT NULL,
+    date_create TIMESTAMP default CURRENT_TIMESTAMP,
+    render_creator_messages BOOLEAN DEFAULT true,
+    render_user_messages BOOLEAN DEFAULT true,
+    date_updated TIMESTAMP default CURRENT_TIMESTAMP
+  );
+  CREATE TABLE IF NOT EXISTS message (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    chat_id UUID REFERENCES chat(id) NOT NULL,
+    sender_id UUID REFERENCES users(id) NOT NULL,
+    message VARCHAR,
+    date_updated TIMESTAMP default CURRENT_TIMESTAMP
+  );
+  `;
+    await client.query(SQL);
+  } else {
+    /**********************************  DEVELOPMENT *******************************/
+    console.log('environment is: development');
+    const SQL = `    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
     DROP TABLE IF EXISTS message;
     DROP TABLE IF EXISTS chat;
@@ -226,73 +225,73 @@ const sync = async () => {
   );
   `;
 
-  await client.query(SQL);
+    await client.query(SQL);
 
-  await Promise.all(
-    Object.values(hardcodedGames).map((each) => games.create(each))
-  );
+    await Promise.all(
+      Object.values(hardcodedGames).map((each) => games.create(each))
+    );
 
-  const _games = await allDataFromAPI;
-  await Promise.all(Object.values(_games).map((each) => games.create(each)));
-  const _gameTypes = [
-    { gametype: 'board' },
-    { gametype: 'card' },
-    { gametype: 'tabletop rpg' },
-  ];
-  await Promise.all(
-    Object.values(_gameTypes).map((each) => gameTypes.create(each))
-  );
+    const _games = await allDataFromAPI;
+    await Promise.all(Object.values(_games).map((each) => games.create(each)));
+    const _gameTypes = [
+      { gametype: 'board' },
+      { gametype: 'card' },
+      { gametype: 'tabletop rpg' },
+    ];
+    await Promise.all(
+      Object.values(_gameTypes).map((each) => gameTypes.create(each))
+    );
 
-  const _users = {
-    admin: {
-      username: 'admin',
-      firstname: 'ad',
-      lastname: 'min',
-      password: 'admin',
-      role: 'ADMIN',
-      email: 'admin@gmail.com',
-      bio: ipsum,
-      latitude: '30.055760',
-      longitude: '-81.500880',
-      avatar: '/assets/avatar.png',
-    },
-    friend: {
-      username: 'friend',
-      firstname: 'fr',
-      lastname: 'iend',
-      password: 'friend',
-      role: 'PLAYER',
-      email: 'friend@gmail.com',
-      bio: 'friend here',
-      latitude: '30.280240',
-      longitude: '-81.724630',
-      avatar: '/assets/avatar.png',
-    },
-    buddy: {
-      username: 'buddy',
-      firstname: 'bud',
-      lastname: 'dy',
-      password: 'buddy',
-      role: 'PLAYER',
-      email: 'buddy@gmail.com',
-      bio: 'buddy here',
-      latitude: '30.303406',
-      longitude: '-81.469178',
-      avatar: '/assets/avatar.png',
-    },
-  };
+    const _users = {
+      admin: {
+        username: 'admin',
+        firstname: 'ad',
+        lastname: 'min',
+        password: 'admin',
+        role: 'ADMIN',
+        email: 'admin@gmail.com',
+        bio: ipsum,
+        latitude: '30.055760',
+        longitude: '-81.500880',
+        avatar: '/assets/avatar.png',
+      },
+      friend: {
+        username: 'friend',
+        firstname: 'fr',
+        lastname: 'iend',
+        password: 'friend',
+        role: 'PLAYER',
+        email: 'friend@gmail.com',
+        bio: 'friend here',
+        latitude: '30.280240',
+        longitude: '-81.724630',
+        avatar: '/assets/avatar.png',
+      },
+      buddy: {
+        username: 'buddy',
+        firstname: 'bud',
+        lastname: 'dy',
+        password: 'buddy',
+        role: 'PLAYER',
+        email: 'buddy@gmail.com',
+        bio: 'buddy here',
+        latitude: '30.303406',
+        longitude: '-81.469178',
+        avatar: '/assets/avatar.png',
+      },
+    };
 
-  await Promise.all(Object.values(_users).map((user) => users.create(user)));
+    await Promise.all(Object.values(_users).map((user) => users.create(user)));
 
-  const userMap = (await users.read()).reduce((acc, user) => {
-    acc[user.username] = user;
-    return acc;
-  }, {});
+    const userMap = (await users.read()).reduce((acc, user) => {
+      acc[user.username] = user;
+      return acc;
+    }, {});
 
-  return {
-    users: userMap,
-  };
-  //}
+    return {
+      users: userMap,
+    };
+  }
 };
 
 module.exports = {
